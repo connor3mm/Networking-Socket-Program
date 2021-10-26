131b155ca32e7d3e73cf91bcee0a163e8c4ab177
tamara <tamara.jankova.2020@strath.uni.ac.uk>
2021-10-26 13:19:01 +0100

    Created sender and receiver classes which extend the TransportLayer abstract class provided

---
+++ src/Sender.java
@@ -0,0 +1,27 @@
+public class Sender extends TransportLayer {
+
+
+    public Sender(String name, NetworkSimulator simulator) {
+        super(name, simulator);
+    }
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void rdt_send(byte[] data) {
+
+    }
+
+    @Override
+    public void rdt_receive(TransportLayerPacket pkt) {
+
+    }
+
+    @Override
+    public void timerInterrupt() {
+
+    }
+}
